02c41493ac4dd3a19c267bed7305e865454376e1
Erik-Jhonatan <erickjhontam03@gmail.com>
2026-06-17 00:35:26 +0000

    Classes criadas atualizadas

--- Vendedor.java
+++ Vendedor.java
@@ -1,43 +1,17 @@
-package model.funcionario;
-
+import java.util.EnumSet;
 import java.util.Set;
 
 public class Vendedor extends Funcionario {
 
-    private double percentualComissao;
-
-    public double getPercentualComissao() {
-        return percentualComissao;
+    public Vendedor(String matricula, String nome, String cpf) {
+        super(matricula, nome, cpf);
     }
 
-    public void setPercentualComissao(double percentualComissao) {
-        this.percentualComissao = percentualComissao;
+    public Set<Permissao> getPermissoes() {
+        return EnumSet.of(
+            Permissao.REALIZAR_VENDA,
+            Permissao.APLICAR_DESCONTO_PADRAO,
+            Permissao.CONSULTAR_ESTOQUE
+        );
     }
-
-    @Override
-    public double calcularSalarioTotal(int mes, int ano) {
-        return salarioBase + calcularComissao(mes, ano);
-    }
-
-    @Override
-    public double calcularComissao(int mes, int ano) {
-        return 0;
-    }
-
-    @Override
-public Set<Permissao> getPermissoes() {
-    return Set.of(
-        Permissao.REALIZAR_VENDA,
-        Permissao.APLICAR_DESCONTO_PADRAO,
-        Permissao.CONSULTAR_ESTOQUE
-    );
 }
-
-    public Vendedor(String m, String n, String cpf, double sBase, double percentualComissao) {
-        super(m, n, cpf, sBase);
-        this.percentualComissao = percentualComissao;
-    }
-
-}
-
-.
